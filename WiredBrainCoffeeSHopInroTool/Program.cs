fc5b05a68aafc799437646d9952a9aa03e75cc68
scott reimonenq <sreimonenq@gmail.com>
2020-04-20 21:51:22 -0700

    Add project files.

---
+++ WiredBrainCoffeeSHopInroTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSHopInroTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Information Tool!");
+        }
+    }
+}
